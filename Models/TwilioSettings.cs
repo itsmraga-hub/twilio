d5162bc6acd909b197604c8fc90c4d23849e224e
itsmraga-hub <itsragamit@gmail.com>
2024-12-14 07:19:52 +0300

    Create Message model

---
+++ Models/TwilioSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace twilio_messager.Models
+{
+    public class TwilioSettings
+    {
+        public string AccountSid { get; set; } = string.Empty;
+        public string AuthToken { get; set; } = string.Empty;
+    }
+
+}
