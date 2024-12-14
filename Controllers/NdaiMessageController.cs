bfdfa0bbe374b79e49f81dce5bdc1a6991840987
itsmraga-hub <itsragamit@gmail.com>
2024-12-14 07:20:17 +0300

    Write a NdaiMessage Controller to send a message

---
+++ Controllers/NdaiMessageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using twilio_messager.Services;
+
+namespace twilio_messager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NdaiMessageController : ControllerBase
+    {
+        private readonly ILogger<NdaiMessageController> _logger;
+        private readonly TwilioService _twilioService;
+
+        public NdaiMessageController(ILogger<NdaiMessageController> logger, TwilioService twilioService)
+        {
+            _logger = logger;
+            _twilioService = twilioService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendMessage(
+            [FromQuery] string to,
+            [FromQuery] string message
+            )
+        {
+            var result = await _twilioService.SendMessage(to, message);
+            return Ok(result);
+        }
+    }
+}
