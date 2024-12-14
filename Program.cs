2198e2c64f8667c6a1bfbb237186e1ddb491501a
itsmraga-hub <itsragamit@gmail.com>
2024-12-14 07:21:10 +0300

    Add Project & Twilio configurations

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using DotNetEnv;
+using twilio_messager.Models;
+using twilio_messager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-
+Env.Load();
 // Add services to the container.
-
+builder.Services.Configure<TwilioSettings>(options =>
+{
+    options.AccountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID")
+                         ?? throw new InvalidOperationException("TWILIO_ACCOUNT_SID is not set in .env");
+    options.AuthToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN")
+                        ?? throw new InvalidOperationException("TWILIO_AUTH_TOKEN is not set in .env");
+});
+builder.Services.AddSingleton<TwilioService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
