398ecc725d47689f2601440dbebd9a71eae61b17
itsmraga-hub <itsragamit@gmail.com>
2024-12-23 11:59:28 +0300

    Add class for SMS Response from the server

---
+++ Models/SMSResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace twilio_messager.Models
+{
+    public class SMSResponse
+    {
+        public string Date { get; set; } = string.Empty;
+        public string From { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
+        public string LinkId { get; set; } = string.Empty; 
+        public string Text { get; set; } = string.Empty;
+        public string NetworkCode { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+
+    }
+}
