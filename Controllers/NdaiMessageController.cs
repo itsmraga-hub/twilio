454070913f06aaaf6b63d440d99fc53cf386ba62
itsmraga-hub <itsragamit@gmail.com>
2024-12-23 12:00:30 +0300

    Add Method route to send a message using africa's talking

--- Controllers/NdaiMessageController.cs
+++ Controllers/NdaiMessageController.cs
@@ -1,20 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AfricasTalkingCS;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
 using twilio_messager.Services;
 
 namespace twilio_messager.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class NdaiMessageController : ControllerBase
+    public class NdaiMessageController(ILogger<NdaiMessageController> logger, TwilioService twilioService) : ControllerBase
     {
-        private readonly ILogger<NdaiMessageController> _logger;
-        private readonly TwilioService _twilioService;
-
-        public NdaiMessageController(ILogger<NdaiMessageController> logger, TwilioService twilioService)
-        {
-            _logger = logger;
-            _twilioService = twilioService;
-        }
+        private readonly ILogger<NdaiMessageController> _logger = logger;
+        private readonly TwilioService _twilioService = twilioService;
 
         [HttpPost]
         public async Task<ActionResult> SendMessage(
@@ -25,5 +21,41 @@ namespace twilio_messager.Controllers
             var result = await _twilioService.SendMessage(to, message);
             return Ok(result);
         }
+
+        [HttpPost("sms")]
+        public async Task<ActionResult> SendMessageAT(
+            [FromQuery] string to,
+            [FromQuery] string message
+            )
+        {
+            string UserName = Environment.GetEnvironmentVariable("APP_USER_NAME")
+                         ?? throw new InvalidOperationException("APP_USER_NAME is not set in .env");
+            string ApiKey = Environment.GetEnvironmentVariable("API_KEY")
+                         ?? throw new InvalidOperationException("API_KEY is not set in .env");
+
+
+            var gateway = new AfricasTalkingGateway(UserName, ApiKey);
+
+            try
+            {
+                //var sms = gateway.SendMessage(recepients, msg);
+                var sms = gateway.SendMessage(to, message);
+                foreach (var res in sms["SMSMessageData"]["Recipients"])
+                {
+                    Console.WriteLine((string)res["number"] + ": ");
+                    Console.WriteLine((string)res["status"] + ": ");
+                    Console.WriteLine((string)res["messageId"] + ": ");
+                    Console.WriteLine((string)res["cost"] + ": ");
+                }
+            }
+            catch (AfricasTalkingGatewayException exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            // var result = await _twilioService.SendMessage(to, message);
+            return Ok();
+        }
+
     }
 }
